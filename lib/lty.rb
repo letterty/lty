4d62f61f02e3022a06a4a336c07202f1ab2cf116
Artur Pyrogovskyi <artpy@pm.me>
2023-06-01 02:33:26 -0700

    the most basic .lty importer

--- lib/lty.rb
+++ lib/lty.rb
@@ -1,8 +1,62 @@
 # frozen_string_literal: true
 
+require 'nokogiri'
 require_relative "lty/version"
 
 module Lty
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Article
+    attr_accessor :head
+    attr_accessor :body
+
+    def initialize(xml)
+      @xml = xml
+
+      self.head = Head.new(xml.at('//lty/head'))
+      self.body = Body.new(xml.at('//lty/body'))
+    end
+
+    def self.import(path_to_lty)
+      xml = File.open(path_to_lty) { |f|
+        Nokogiri::XML(f)
+      }
+
+      return self.new(xml)
+    end
+  end
+
+  class Head
+    attr_accessor :title
+    attr_accessor :lead
+
+    def initialize(xml)
+      @xml = xml
+
+      self.title = xml.at('title').text
+      self.lead  = xml.at('lead').text
+    end
+  end
+
+  class Body
+    attr_accessor :paragraphs
+
+    def initialize(xml)
+      @xml = xml
+
+      self.paragraphs = xml.xpath('p').map { |pxml|
+        Paragraph.new(pxml)
+      }
+    end
+  end
+
+  class Paragraph
+    attr_accessor :content
+
+    def initialize(xml)
+      @xml = xml
+
+      self.content = xml.text
+    end
+  end
 end
--- spec/lty_spec.rb
+++ spec/lty_spec.rb
@@ -1,11 +1,35 @@
 # frozen_string_literal: true
 
+EXAMPLE_PATH = File.join(__dir__, 'fixtures/example.lty')
+
 RSpec.describe Lty do
   it "has a version number" do
     expect(Lty::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe "importing" do
+    let(:article) { Lty::Article.import(EXAMPLE_PATH) }
+
+    describe "head" do
+      let(:head) { article.head }
+
+      it "parses title" do
+        expect(head.title).to eq('How do we learn to fly?')
+      end
+
+      it "parses lead" do
+        expect(head.lead).to eq('Being a bird is a difficult job. It starts with learning to fly.')
+      end
+    end
+
+    describe "body" do
+      let(:body) { article.body }
+
+      it "parses paragraphs" do
+        expect(body.paragraphs[0].content).to eq('Where are the birds? They are flying.')
+        expect(body.paragraphs[1].content).to eq('Flying high in the sky, it\'s blue as far as your eyes can see.')
+        expect(body.paragraphs[2].content).to eq('And it\'s also fun, of course.')
+      end
+    end
   end
 end
